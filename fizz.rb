b40a50f8d17874a4759089c0a66f148c9d165f0b
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-18 01:38:18 +0000

    automatically backed up by learn

--- fizz.rb
+++ fizz.rb
@@ -1,5 +1,15 @@
-#fizz 
+#fizz = 3
 
-#buzz 
+#buzz = 5
 
-#f
+#fizzbuzz % by 3 and 5
+
+def fizzbuzz(integer)
+  if integer % 3 == 0
+    "fizz"
+  elsif integer % 5 == 0
+    "buzz" 
+  else integer % 3 == 0 && integer % 5 == 0 
+    "fizzbuzz" 
+  end 
+end 
